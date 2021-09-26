e528ae0526a630e880b262aa1365572f63037668
VaderSteve76 <alverson.s@yahoo.com>
2021-09-26 19:48:08 -0400

    Restructured useEffect, search widget complete

--- widgets-hooks/src/components/Search.js
+++ widgets-hooks/src/components/Search.js
@@ -4,10 +4,19 @@ import axios from 'axios';
 const Search = () => {
 
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]); 
 
   useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
@@ -15,26 +24,13 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         }
-    });
-    setResults(data.query.search);
-  };
-
-  if(term && !results.length) {
-    search();
-  } else {
-    const timeoutId = setTimeout(() => {
-      if(term) {
-        search();
-      }
-    }, 800);
-    return () => {
-      clearTimeout(timeoutId);
+      });
+      setResults(data.query.search);
     };
-  }
-  
-}, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
