3504c877935d2430bf11e9b59d786a61473158a5
VaderSteve76 <alverson.s@yahoo.com>
2021-10-12 23:10:54 -0400

    Form for translation

--- widgets-hooks/src/App.js
+++ widgets-hooks/src/App.js
@@ -39,18 +39,19 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      <Translate />
+      {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
       {showDropdown ? (
         <Dropdown 
           selected={selected} 
           onSelectedChange={setSelected}
           options={options} 
         /> 
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
