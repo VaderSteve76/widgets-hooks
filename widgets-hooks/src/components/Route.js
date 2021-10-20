55eed7d0858d4bceae8b237a2ee04ae504ec8f56
VaderSteve76 <alverson.s@yahoo.com>
2021-10-20 19:13:15 -0400

    Fixed link functionality

--- widgets-hooks/src/components/Route.js
+++ widgets-hooks/src/components/Route.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+
+  const [curPath, setCurPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocChange = () => {
+      setCurPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', onLocChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocChange);
+    };
+  }, []);
+
+  return curPath === path ? children : null;
 };
 
 export default Route;
